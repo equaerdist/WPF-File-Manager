23a1aa6347ae465cea39fd645399856090e7322e
equaer <vriyazzz1001@gmail.com>
2024-02-09 23:18:42 +0300

    Добавление автоскролла на списке при пемещении объекта

--- Views/FileManager/FileManager.xaml.cs
+++ Views/FileManager/FileManager.xaml.cs
@@ -26,6 +26,8 @@ namespace fileChanger.Views.FileManager
             InitializeComponent();
             List_left.MouseDown += List_MouseDown;
             List_left.MouseMove += List_MouseMove;
+            List_right.MouseDown += List_MouseDown;
+            List_right.MouseMove += List_MouseMove;
         }
         Point startPoint;
 
@@ -33,21 +35,57 @@ namespace fileChanger.Views.FileManager
         {
             startPoint = e.GetPosition(null);
         }
+        private T? FindChildrenFromType<T>(DependencyObject element) where T : UIElement
+        {
+            if (element == null)
+                return default(T);
+            if (element is T)
+                return (T)element;
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(element); i++)
+            {
+                var result = FindChildrenFromType<T>(VisualTreeHelper.GetChild(element, i));
+                if (!Equals(default(T), result))
+                    return result;
+            }
+            return default(T);
+        }
+        private void list_DragOver(object sender, DragEventArgs e)
+        {
+            var source = e.Source as ListBox;
 
+            if (source == null)
+                return;
+            var point = e.GetPosition(source);
+
+            var scrollViewer = FindChildrenFromType<ScrollViewer>(source);
+            if (scrollViewer == null)
+                return;
+
+            if (point.Y < SystemParameters.MinimumVerticalDragDistance)
+            {
+                scrollViewer.LineUp();
+            }
+            else if (point.Y > Math.Abs(source.ActualHeight - SystemParameters.MinimumVerticalDragDistance))
+            {
+                scrollViewer.LineDown();
+            }
+        }
         void List_MouseMove(object sender, MouseEventArgs e)
         {
             // Get the current mouse position
             Point mousePos = e.GetPosition(null);
             Vector diff = startPoint - mousePos;
-
+            var target = (ListBox)sender;
             if (e.LeftButton == MouseButtonState.Pressed
                 && (
                     Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance
-                    || Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance)
+                    || Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance
+                    && target.SelectedItem is not null)
                 )
             {
-                DataObject dragData = new DataObject(DataFormats.FileDrop, List_left.SelectedItems);
-                DragDrop.DoDragDrop(List_left, dragData, DragDropEffects.Copy);
+               
+                DataObject dragData = new DataObject(DataFormats.FileDrop, target.SelectedItems);
+                DragDrop.DoDragDrop(target, dragData, DragDropEffects.Copy);
             }
         }
 
@@ -58,6 +96,14 @@ namespace fileChanger.Views.FileManager
                 return;
             var dataContext = (FileManagerViewModel)DataContext;
             dataContext.MoveCommand.Execute(new DragDropInfo(ViewModels.MainWindowViewMod.Direction.Left, item));
+        }
+        private void List_left_Drop(object sender, DragEventArgs e)
+        {
+            var item = e.Data.GetData(DataFormats.FileDrop) as IList<object>;
+            if (item is null)
+                return;
+            var dataContext = (FileManagerViewModel)DataContext;
+            dataContext.MoveCommand.Execute(new DragDropInfo(ViewModels.MainWindowViewMod.Direction.Right, item));
         }
     }
 }
